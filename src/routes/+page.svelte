bc5ebf125d60cba45f898e0bc48239b69410936c
Joshua Hicks <joshua.brock.hicks@gmail.com>
2023-08-25 16:31:43 -0700

    daisy UI now works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1 class="underline text-green-400">Welcome to SvelteKit</h1>
+<button class="btn">Button</button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
